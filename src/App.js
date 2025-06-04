247cb41d4e32bf8c4268441518c5d3749902937b
Zi Xiang <zixiang150193@hotmail.com>
2025-06-04 21:23:39 +0800

    Add backend link for easier access.

--- src/App.js
+++ src/App.js
@@ -238,9 +238,9 @@ class App extends Component {
       <div className="App">
         <div className="info">
           {/* <hr className="line"></hr> */}
-          <p>By Zack Ng | Version 1.1 </p>
-          <p>Last Updated: 4 June 2025</p>
-          <p>Clarifai API | Neon PostgreSQL | React | Node.JS</p>
+          <p>By Zack Ng | Version 1.1 | Last Updated: 4 June 2025 </p>
+          <p>Clarifai API | Neon PostgreSQL | React | Node.JS | Render</p>
+          <p>Click <a href="https://www.example.com">'here'</a>to start the backend.</p>
           {/* <hr className="line"></hr> */}
         </div>
         <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange} />
